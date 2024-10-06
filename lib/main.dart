20398b82ee76eafbd3ba40780ae9e6c071c03637
hjangir081 <hjangir081@gmail.com>
2024-10-06 22:47:49 +0530

    commit with changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:code_chef/src/utils/locator.dart';
 import 'package:flutter/material.dart';
 
-class CodeChef extends StatelessWidget {
-  const CodeChef({super.key});
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeApp();
+  runApp(const CloudChef());
+}
+
+class CloudChef extends StatelessWidget {
+  const CloudChef({super.key});
 
   @override
   Widget build(BuildContext context) {
